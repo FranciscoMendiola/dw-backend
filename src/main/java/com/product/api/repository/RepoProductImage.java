5fa0b41a824f4b1825a75e12c28ad520a72040d0
FranciscoMendiola <franciscomendiola9@ciencias.unam.mx>
2025-04-04 23:52:22 -0600

    feat: agregar autenticacion con jwt

--- src/main/java/com/product/api/repository/RepoProductImage.java
+++ src/main/java/com/product/api/repository/RepoProductImage.java
@@ -11,6 +11,11 @@ import com.product.api.entity.ProductImage;
 @Repository
 public interface RepoProductImage extends JpaRepository<ProductImage, Integer> {
 
-    @Query(value = "SELECT * FROM product_image WHERE product_id = :product_id AND status = 1", nativeQuery = true)
+    @Query(value = "SELECT pi.* " +
+            "FROM product_image pi " +
+            "INNER JOIN product p ON pi.product_id = p.product_id " +
+            "WHERE pi.product_id = :product_id " +
+            "AND pi.status = 1 " +
+            "AND p.status = 1", nativeQuery = true)
     public List<ProductImage> findByProductId(Integer product_id);
 }
